375a77e4011fa1854024bc8d921b08b11fe2569b
ssk63 <ssk@dsmn8.com>
2022-10-06 11:28:01 +0530

    create shared lib

---
+++ libs/shared/src/lib/components/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'dsmn8-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class ButtonComponent {
+  @Input() label: string | undefined;
+  @Input() ariaLabel: string | undefined;
+  @Input() isPrimary = true;
+  @Input() isDisabled = false;
+}
