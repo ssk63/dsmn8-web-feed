4528c9e7868afd8bd270e64708805e01c153430e
ssk63 <ssk@dsmn8.com>
2022-10-07 08:32:32 +0530

    refactor feed e2e

--- apps/feed-e2e/src/e2e/components/feed-header/feed-header.component.cy.ts
+++ apps/feed-e2e/src/e2e/components/feed-header/feed-header.component.cy.ts
@@ -1,6 +1,6 @@
 describe('feed', () => {
   beforeEach(() =>
-    cy.visit('/iframe.html?id=feedpostheadercomponent--primary&args=text;')
+    cy.visit('/iframe.html?id=feedheadercomponent--primary&args=text;')
   );
   it('should render the component', () => {
     cy.get('dsmn8-feed-header').should('exist');
