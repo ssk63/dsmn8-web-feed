e1422c4517b5d36b91c5a990de2484ab8ee21509
ssk63 <ssk@dsmn8.com>
2022-10-12 17:20:24 +0530

    Added Feed header and footer components Cypress e2e

--- apps/feed-e2e/src/e2e/components/feed-header/feed-header.component.cy.ts
+++ apps/feed-e2e/src/e2e/components/feed-header/feed-header.component.cy.ts
@@ -1,8 +1,21 @@
 describe('feed', () => {
-  beforeEach(() =>
-    cy.visit('/iframe.html?id=feedheadercomponent--header-text&args=text;')
-  );
   it('should render the component', () => {
+    cy.visit('/iframe.html?id=feedheadercomponent--header-text&args=text;');
     cy.get('dsmn8-feed-header').should('exist');
+  });
+
+  it('should render the header', () => {
+    cy.get('.feed-header__details > h1').contains(
+      `Help promote DSMN8's post on Linkedin`
+    );
+  });
+
+  it('should render the text', () => {
+    cy.visit(
+      '/iframe.html?id=feedheadercomponent--header-text&args=text:By+liking+and+re-sharing+this+post+,+you+can+help+boost+the+organic+reach+of+the+content;'
+    );
+    cy.get('.feed-header > p').contains(
+      `By liking and re-sharing this post, you can help boost the organic reach of the content`
+    );
   });
 });
